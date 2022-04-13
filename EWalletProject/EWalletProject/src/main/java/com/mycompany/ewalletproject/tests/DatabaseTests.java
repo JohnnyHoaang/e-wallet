227b9b1a1186600b20e4ff3f7ffc850585ad0ae0
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-04-13 19:46:35 -0400

    removed non necessary button and added tests files

--- EWalletProject/EWalletProject/src/main/java/com/mycompany/ewalletproject/tests/DatabaseTests.java
+++ EWalletProject/EWalletProject/src/main/java/com/mycompany/ewalletproject/tests/DatabaseTests.java
@@ -3,7 +3,7 @@ package com.mycompany.ewalletproject.tests;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class CreditCardTests {
+public class DatabaseTests{
     @Test
     public void checkFundsTrueTest(){
         assertEquals(1, 1);
--- EWalletProject/EWalletProject/src/main/java/com/mycompany/ewalletproject/tests/WalletTests.java
+++ EWalletProject/EWalletProject/src/main/java/com/mycompany/ewalletproject/tests/WalletTests.java
@@ -3,9 +3,10 @@ package com.mycompany.ewalletproject.tests;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class CreditCardTests {
+public class WalletTests {
     @Test
     public void checkFundsTrueTest(){
         assertEquals(1, 1);
     }
 }
+
