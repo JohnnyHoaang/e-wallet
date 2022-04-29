e41974c119f7112c5ac53b4ae3eace564d6ef636
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-04-29 18:06:16 -0400

    push to merge

--- EWalletProject/EWalletProject/src/main/java/com/mycompany/ewalletproject/storage/Database.java
+++ EWalletProject/EWalletProject/src/main/java/com/mycompany/ewalletproject/storage/Database.java
@@ -3,30 +3,19 @@ package com.mycompany.ewalletproject.storage;
 import java.util.ArrayList;
 
 public class Database{
-    ArrayList<Wallet> db = new ArrayList<Wallet>();
+    private Wallet db = new Wallet();
     private static Database self = new Database();
     public static Database get(){return self;}
 
-    public ArrayList<Wallet> getDB(){
+    public Wallet getDB(){
         return this.db;
     }
     public void save(Wallet wallet){
-        
-        for (Wallet w: db){
-            if (w.getID() == wallet.getID()){
-                w = wallet;
-                return;
-            }
-        }
-        db.add(wallet);
+        // insert data to database
     }
     public Wallet load(int id){
+        //fetch data from database
         Wallet wallet = null;
-        for (Wallet w: db){
-            if (w.getID() == id){
-                wallet = w;
-            }
-        }
         return wallet;
     }
 }
