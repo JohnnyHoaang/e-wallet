22f58f9b9f0d9a185ee0ad0494c847959d2e7bd3
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-05-09 18:35:42 -0400

    added test case for loading

--- EWalletProject/EWalletProject/src/test/java/tests/CashTests.java
+++ EWalletProject/EWalletProject/src/test/java/tests/CashTests.java
@@ -9,22 +9,22 @@ public class CashTests {
     @Test
     public void payTestTrue(){
         Cash cash = new Cash(100);
-        boolean check = cash.pay(99);
-        boolean secondCheck = cash.pay(1);
+        boolean check = cash.withdraw(99);
+        boolean secondCheck = cash.withdraw(1);
         assertEquals(true, check);
         assertEquals(true, secondCheck);
     }
     @Test
     public void payTestFalse(){
         Cash cash = new Cash(100);
-        boolean check = cash.pay(140);
+        boolean check = cash.withdraw(140);
         assertEquals(false, check);
     }
     @Test
     public void payNegativeAmountTest(){
         Cash cash = new Cash(100);
         try {
-            cash.pay(-1);
+            cash.withdraw(-1);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -33,7 +33,7 @@ public class CashTests {
     public void payHighAmountFalseTest(){
         Cash cash = new Cash(100);
         try {
-            cash.pay(150);
+            cash.withdraw(150);
         } catch (Exception e) {
             e.printStackTrace();
         }
